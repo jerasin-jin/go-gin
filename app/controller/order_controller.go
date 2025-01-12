238b6dff48ca2302550e2fe390eba92605b2ba50
jerasin-ch <jerasin.ch@centrilliontech.co.th>
2025-01-12 14:35:26 +0700

    feat: add crud role_info with permission_info an refactor code

--- app/controller/order_controller.go
+++ app/controller/order_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/Jerasin/app/response"
 	"github.com/Jerasin/app/service"
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,7 @@ type OrderController struct {
 
 type OrderControllerInterface interface {
 	CreateOrder(c *gin.Context)
+	GetAllProducts(c *gin.Context)
 }
 
 func OrderControllerInit(orderSvc service.OrderServiceInterface) *OrderController {
@@ -19,9 +21,9 @@ func OrderControllerInit(orderSvc service.OrderServiceInterface) *OrderControlle
 	}
 }
 
-// @Summary CreateOrder
+// @Summary Create Order
 // @Schemes
-// @Description CreateOrder
+// @Description Create Order
 // @Tags Order
 //
 // @Param request body request.OrderRequest true "query params"
@@ -33,4 +35,26 @@ func OrderControllerInit(orderSvc service.OrderServiceInterface) *OrderControlle
 // @Router /orders [post]
 func (o OrderController) CreateOrder(c *gin.Context) {
 	o.svc.CreateOrder(c)
+}
+
+// @Summary Get Order List
+// @Schemes
+// @Description Get Order List
+// @Tags Order
+//
+// @Param   page         query     int        false  "int valid"
+// @Param   pageSize         query     int        false  "int valid"
+// @Param   sortField         query     string        false  "string valid"
+// @Param   sortValue         query     string        false  "string valid"
+//
+//	@Success		200	{object}	response.OrderPagination
+//
+// @Security Bearer
+//
+// @Router /orders [get]
+func (o OrderController) GetAllProducts(c *gin.Context) {
+	query := CreatePagination(c)
+	order := response.Order{}
+
+	o.svc.GetPaginationOrder(c, query.page, query.pageSize, query.search, query.sortField, query.sortValue, order)
 }
